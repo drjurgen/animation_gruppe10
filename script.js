7e11d1e07bb2df93b4b82b5b8045140dc58414f7
Laura <lauraragnars@Lauras-MacBook-Pro-2.local>
2019-11-18 11:35:17 +0100

    if sætning virker!!!!!!!!!!!

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ window.addEventListener("load", sidenLoades);
 
 let points;
 let liv;
+let randomtal;
+
+
+randomtal = Math.floor(Math.random() * 7) + 1;
+console.log(randomtal);
+
 
 function sidenLoades() {
     console.log("sidenLoades");
@@ -57,12 +63,22 @@ function startGame() {
     points = 0;
 
     // Eventlisteners til good og bad knapper - click
+    document.querySelector("#billede1").addEventListener("click", goodSelector);
+    document.querySelector("#billede2").addEventListener("click", goodSelector);
+    document.querySelector("#billede3").addEventListener("click", goodSelector);
+    document.querySelector("#billede4").addEventListener("click", goodSelector);
+    document.querySelector("#billede5").addEventListener("click", goodSelector);
+    document.querySelector("#billede6").addEventListener("click", goodSelector);
+    document.querySelector("#billede7").addEventListener("click", goodSelector);
+
 
     // Baggrundsmusik spilles
 
     // Timer gaar i gang
     document.querySelector("#time_board_fill").classList.add("timer");
 
+    // et random billede vises
+    document.querySelector("#billede" + randomtal).classList.remove("hide");
 
 }
 
@@ -70,20 +86,68 @@ function goodSelector() {
     console.log("goodSelector");
 
     // if sætning som tjekker om man har valgt rigtigt og den rigtige figur vises
+    if (this.classList.contains("good")) {
+        console.log("du fik point")
 
-    // mister liv hvis man vælger forkert
+        // faar point hvis man vælger rigtigt
+        points++;
 
-    // faar point hvis man vælger rigtigt
+        document.querySelector("#score_points").innerHTML = points;
+    } else {
+        console.log("du fik ikke points")
+
+        document.querySelector("#hjerte" + liv).classList.add("grey");
+
+        // mister liv hvis man vælger forkert
+        liv--;
+    }
+
+    document.querySelector("#billede1").removeEventListener("click", goodSelector);
+    document.querySelector("#billede2").removeEventListener("click", goodSelector);
+    document.querySelector("#billede3").removeEventListener("click", goodSelector);
+    document.querySelector("#billede4").removeEventListener("click", goodSelector);
+    document.querySelector("#billede5").removeEventListener("click", goodSelector);
+    document.querySelector("#billede6").removeEventListener("click", goodSelector);
+    document.querySelector("#billede7").removeEventListener("click", goodSelector);
 
     // Billedet forsvinder - animation
+    this.classList.add("hide");
 
     // if sætning om der er liv tilbage eller ikke
+
+    if (liv >= 1) {
+        respawn();
+
+    } else {
+        gameOver();
+    }
+
+
 }
 
 function badSelector() {
     console.log("badSelector");
 
     // if sætning som tjekker om man har valgt rigtigt og den rigtige figur vises
+    if (this.classList.contains("good")) {
+        console.log("du fik ikke point")
+
+        document.querySelector("#hjerte" + liv).classList.add("grey");
+
+        // faar point hvis man vælger rigtigt
+        liv--;
+
+    } else {
+        console.log("du fik points")
+
+
+        // mister liv hvis man vælger forkert
+        points++;
+
+        document.querySelector("#score_points").innerHTML = points;
+
+    }
+
 
     // mister liv hvis man vælger forkert
 
@@ -92,16 +156,29 @@ function badSelector() {
     // Billedet forsvinder - animation, add class "hide"
 
     // if sætning om der er liv tilbage eller ikke
+
+    this.removeEventListener("click", badSelector);
+
 }
 
 function respawn() {
     console.log("respawn");
 
-    // Der vises et nyt random billede
+    randomtal = Math.floor(Math.random() * 7) + 1;
 
+    // Der vises et nyt random billede
     // hide classen fjernes
+    document.querySelector("#billede" + randomtal).classList.remove("hide");
+
 
     // animation paa billede
+    document.querySelector("#billede1").addEventListener("click", goodSelector);
+    document.querySelector("#billede2").addEventListener("click", goodSelector);
+    document.querySelector("#billede3").addEventListener("click", goodSelector);
+    document.querySelector("#billede4").addEventListener("click", goodSelector);
+    document.querySelector("#billede5").addEventListener("click", goodSelector);
+    document.querySelector("#billede6").addEventListener("click", goodSelector);
+    document.querySelector("#billede7").addEventListener("click", goodSelector);
 
 
 }
