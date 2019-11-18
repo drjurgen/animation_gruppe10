d30453c515fed3648bf2f03bb3efb3c71b6723b2
Laura <lauraragnars@Lauras-MacBook-Pro-2.local>
2019-11-18 10:50:21 +0100

    Start, info og spilskærm virker i java

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 window.addEventListener("load", sidenLoades);
 
+let points;
+let liv;
+
 function sidenLoades() {
     console.log("sidenLoades");
 
     // startskærm vises
+    document.querySelector("#start").classList.remove("hide");
 
     // Eventlisteners til knapper
+    document.querySelector("#startknap").addEventListener("click", infoScreen);
+
+    // Timer er stop
+    document.querySelector("#time_board_fill").classList.remove("timer");
+
 
 
 }
@@ -14,27 +23,45 @@ function infoScreen() {
     console.log("infoScreen");
 
     // Infoskærm vises
+    document.querySelector("#gameinfo").classList.remove("hide");
 
     // Startskærm hides
+    document.querySelector("#start").classList.add("hide");
+
 
     // Eventlisterner til hele skærmen - click
+    document.querySelector("#gameinfo").addEventListener("click", startGame);
+
+    // timer nulstil
+    document.querySelector("#time_board_fill").classList.remove("timer");
+
+
+
 }
 
 
 function startGame() {
     console.log("startGame");
 
+    document.querySelector("#gameinfo").removeEventListener("click", startGame);
+
+
     // Infoskærm hides
+    document.querySelector("#gameinfo").classList.add("hide");
 
     // Spilskærm vises
+    document.querySelector("#game").classList.remove("hide");
 
     // Liv og point og timer nultilles
+    liv = 3;
+    points = 0;
 
     // Eventlisteners til good og bad knapper - click
 
     // Baggrundsmusik spilles
 
     // Timer gaar i gang
+    document.querySelector("#time_board_fill").classList.add("timer");
 
 
 }
